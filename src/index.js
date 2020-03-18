23b565c1a1e951a036a14caf9e8c6c62ed2b99ba
ali <ali@iMac-de-Elodie.fritz.box>
2020-03-18 22:13:19 +0100

    employees Project

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import {
+    Request
+} from "./requests";
+import {
+    UI
+} from "./ui";
+//selecting elements
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+const ui = new UI();
+
+
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener("DOMContentLoaded", getAllEmployees);
+    form.addEventListener("submit", addEmployee)
+}
+
+function getAllEmployees() {
+    request.get()
+        .then(employees => {
+            ui.addAllEmployeeToUI(employees)
+        })
+        .catch(err => console.log(err));
+}
+
+function addEmployee(e) {
+
+    const employeeName = nameInput.value.trim();
+    const employeeDepartment = departmentInput.value.trim();
+    const employeeSalary = salaryInput.value.trim();
+
+    if (employeeName === "" || employeeDepartment === "" || employeeSalary === "") {
+        alert("Plz fill out the fields")
+    } else {
+        request.post({
+                name: employeeName,
+                department: employeeDepartment,
+                salary: Number(employeeSalary)
+            }).then(employee => {
+                ui.addEmployeeToUI(employee);
+            })
+            .catch(err => console.log(err))
+    }
+
+    ui.clearInputs();
+    e.preventDefault();
+}
+
+// request.get()
+// .then(employees => console.log(employees))
+// .catch(err => console/log(err));
+
+// request.post({
+//     name:"Radamel Falcao",
+//     department: "Forvet",
+//     salary: 1000
+// }).then(employee => console.log(employee))
+// .catch(err => console.log(err));
+
+// request.put(1,{
+//     name:"Radamel Falcao",
+//     department: "defans",
+//     salary: 2000
+// }).then(employee => console.log(employee))
+// .catch(err => console.log(err));
+
+
+// request.delete(3)
+// .then((message => console.log(message)))
+// .catch(err=> console.log(err))
